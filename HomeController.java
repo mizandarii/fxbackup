67295f0a8c195ea9be50da8771936fea8e35866c
mizandarii <132040885+mizandarii@users.noreply.github.com>
2024-04-28 22:23:47 +0300

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fxbookshopnew2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author user
+ */
+public class HomeController implements Initializable {
+    
+    
+    FXBookShopNew2 app;
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    public FXBookShopNew2 getApp() {
+        return app;
+    }
+
+    public void setApp(FXBookShopNew2 app) {
+        this.app = app;
+    }
+    
+}
